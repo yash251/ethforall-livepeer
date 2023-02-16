fd7f7011a816b34f87b6111c8f75c1143947c2be
Yash Jagtap <jagtapyash2512@gmail.com>
2023-02-16 13:36:09 +0530

    button and asset import

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import {Button, Asset} from '../components';
 
 export default function Home() {
   return (
     <div className='flex flex-col justify-center items-center h-screen'>
       <h1 className='text-6xl font-bold'>NFT Connect</h1>
+      <Button />
+      <Asset />
     </div>
   )
 }
