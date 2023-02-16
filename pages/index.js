6fd4098ce20200966e42dbe37bdfd1c0132891fc
Yash Jagtap <jagtapyash2512@gmail.com>
2023-02-16 14:52:21 +0530

    button blpt

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,9 @@ export default function Home() {
   return (
     <div className='flex flex-col justify-center items-center h-screen'>
       <h1 className='text-6xl font-bold'>NFT Connect</h1>
-      <Button />
+      <Button>
+        Click to upload
+      </Button>
       <Asset />
     </div>
   )
