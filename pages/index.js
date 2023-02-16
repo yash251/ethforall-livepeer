7f3e985aa2d672bbe4a568bc74064f6ca3d11b82
Yash Jagtap <jagtapyash2512@gmail.com>
2023-02-16 15:35:04 +0530

    input for video file

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,11 @@ import {Button, Asset} from '../components';
 export default function Home() {
   return (
     <div className='flex flex-col justify-center items-center h-screen'>
-      <h1 className='text-6xl font-bold'>NFT Connect</h1>
+      <h1 className='text-8xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-fuchsia-500 to-pink-500 mb-4'>NFT Connect</h1>
       <Button>
         Click to upload
       </Button>
+      <input type={'file'} accept='video/*' className='hidden' /> 
       <Asset />
     </div>
   )
