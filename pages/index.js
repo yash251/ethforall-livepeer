18c472040af9c872037c3d6813443e170c28cd9f
Yash Jagtap <jagtapyash2512@gmail.com>
2023-02-17 10:03:51 +0530

    upload asset to livepeer

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,27 @@
 import { useState, useRef } from 'react';
-import {Button, Asset} from '../components';
+import { Button, Asset } from '../components';
+
+import { useCreateAsset } from '@livepeer/react';
+
 
 export default function Home() {
   const ref = useRef(null);
 
   const [asset, setAsset] = useState(null);
+
+  const {
+    mutate: createAsset,
+    data: assets,
+    status,
+    progress,
+    error,
+  } = useCreateAsset(
+    asset
+      ? {
+          sources: [{ name: asset.name, file: asset }],
+        }
+      : null,
+  );
 
   const chooseFile = () => { 
     ref.current.click();
@@ -18,11 +35,16 @@ export default function Home() {
     setAsset(file);
   };
 
+  const uploadAsset = async () => { 
+    console.log('Uploading asset');
+    await createAsset();
+  }
+
   return (
     <div className='flex flex-col justify-center items-center h-screen'>
       <h1 className='text-8xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-fuchsia-500 to-pink-500 mb-4'>NFT Connect</h1>
       <Button
-        onClick={chooseFile}
+        onClick={asset ? uploadAsset : chooseFile}
       >
         {asset ? "Upload a file" : "Choose a file"}
       </Button>
